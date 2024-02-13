85082eaa1690d6179ce78082a154e51ae8fd5261
Hoang Anh Tuan <122135362+tuanx18@users.noreply.github.com>
2024-02-13 23:05:35 +0700

    update chatbot 13.2.2024

--- assistbot.py
+++ assistbot.py
@@ -1,10 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QTextBrowser
 from PyQt5.QtGui import QFont
+import spacy
+from spacy.matcher import Matcher
+import json
+from datetime import datetime, timedelta
+import pytz
+
+# Set the timezone to GMT = 0
+timezone = pytz.timezone('Etc/GMT')
+
+# Get the current time in the specified timezone
+current_time = datetime.now(timezone)
 
 class ChatWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.nlp = spacy.load("en_core_web_sm")
 
         self.setWindowTitle("AssistBot by tuanx18")
         self.resize(1000, 800)
@@ -57,8 +70,211 @@ class ChatWindow(QMainWindow):
             self.input_box.clear()
 
     def generate_bot_response(self):
-        # Replace this with your actual logic to generate bot responses
-        return "///"
+        input_text = self.input_box.text()
+
+        # Process input text using spaCy
+        doc = self.nlp(input_text)
+
+        # INPUT => OUTPUT
+
+        # Force Quit using QProgram or quitprogram
+        if any("qprogram" in token.text.lower() for token in doc) or any("quitprogram" in token.text.lower() for token in doc):
+            self.quit_application()
+
+        # City + Weather
+        elif any(token.text.lower() == "weather" for token in doc):
+            location = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # GPE: Geo-Political Entity (Location)
+                    location = ent.text.lower()
+                    break
+
+            if location:
+                weather_info = self.get_weather_info(location)
+                if weather_info:
+                    return f"The weather in {location.capitalize()} is {weather_info['weather']}, {weather_info['temperature']} Celcius Degree"
+                else:
+                    return f"Sorry, I couldn't find the weather information for {location.capitalize()}"
+            else:
+                return "I'm sorry, I didn't quite catch the location. Can you please specify it?"
+
+        # Country + capital
+        elif any(token.text.lower() == "capital" for token in doc):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    return f"The capital of {country.capitalize()} is {country_info['capital'].capitalize()}."
+                else:
+                    return f"Sorry, I cannot find the capital of {country}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + gdp
+        elif any(token.text.lower() == "gdp" for token in doc):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    return f"Up-to-date on February 2024, the recorded GDP or Gross Domestic Product of {country.capitalize()} is {country_info['GDP']}."
+                else:
+                    return f"Sorry, I cannot find the GDP of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + size/area
+        elif any(token.text.lower() == "area" for token in doc) or any(token.text.lower() == "size" for token in doc):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    area = country_info['area'].replace(',', '')        # Remove comma
+                    area_number = int(area.split()[0])                  # Convert to numeric
+                    if area_number >= 1000000:
+                        return f"This country is so HUGE! The living area of {country} is {country_info['area']}."
+                    elif 100000 <= area_number < 1000000:
+                        return f"{country} has the living land area of {country_info['area']}, " \
+                               f"which is a moderate-sized country."
+                    else:
+                        return f"The size of this country is a bit modest, {country} has the area of {country_info['area']}"
+                else:
+                    return f"Sorry, I cannot find the GDP of {country}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + Population
+        elif any(token.text.lower() == "population" for token in doc):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    if country_info['population'] > 1000000000:
+                        return f"The population of {country} is {country_info['population']}. " \
+                               f"This is one of a few countries with more than 1 billion people."
+                    else:
+                        return f"The population of {country} is {country_info['population']}."
+                else:
+                    return f"Sorry, I cannot find the population of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + Timezome
+        elif any(token.text.lower() == "time" for token in doc) or any(token.text.lower() == "timezone" for token in doc):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    country_gmt = country_info['timezone']
+                    new_time = current_time + timedelta(hours=country_gmt)
+                    new_time = new_time.strftime("%H:%M, %d-%m-%Y")
+                    return f"Currently, the time of this country is {new_time}, its timezone is GMT+{country_gmt}"
+                else:
+                    return f"Sorry, I cannot find the time of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + Sea Boundary
+        elif (any(token.lemma_.lower() == "sea" for token in doc) or any(token.lemma_.lower() == "ocean" for token in doc))\
+                and (any(token.text.lower() == "border" for token in doc)
+                     or any(token.text.lower() == "boundary" for token in doc)):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    if country_info['sea_boundary'] == 0:
+                        return f"{country.capitalize()} does not have any sea boundary at all. How sad!"
+                    else:
+                        return f"{country.capitalize()}'s length of the sea borderline is {country_info['sea_boundary']}"
+                else:
+                    return f"Sorry, I cannot find the time of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + Best Ally
+        elif any(token.lemma_.lower() == "good" for token in doc) \
+                and (any(token.text.lower() == "ally" for token in doc) or any(token.text.lower() == "alliance" for token in doc)):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    return f"The best alliance of this country is {country_info['best_ally']}"
+                else:
+                    return f"Sorry, I cannot find the time of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Country + AVG Income
+        elif any(token.lemma_.lower() == "average" for token in doc) \
+                and (any(token.lemma_.lower() == "income" for token in doc) or any(token.lemma_.lower() == "earn" for token in doc)):
+            country = None
+            for ent in doc.ents:
+                if ent.label_ == "GPE":  # Geo-Political Entity (Location)
+                    country = ent.text.lower()
+                    break
+
+            if country:
+                country_info = self.get_country_info(country)
+                if country_info:
+                    return f"The average income of a {country.capitalize()} person is {country_info['average_income']} USD."
+                else:
+                    return f"Sorry, I cannot find the time of {country.capitalize()}, the entity might not be a country."
+            else:
+                return "I'm sorry, I didn't quite catch the country. Can you please specify it?"
+
+        # Hello / Hi
+        elif any(token.text.lower() in ["hello", "hi"] for token in doc):
+            return "Hello my friend, welcome back. This is AssistBot 1.0"
+
+        else:
+            return "I'm sorry, I didn't quite catch that. Can you please rephrase?"
+
+
+    def get_weather_info(self, location):
+        with open("res.json", "r") as json_file:
+            data = json.load(json_file)
+            weather_data = data.get("weather")
+            return weather_data.get(location.lower())
+
+    def get_country_info(self, country):
+        with open("res.json", "r") as json_file:
+            data = json.load(json_file)
+            country_data = data.get("country")
+            return country_data.get(country.lower())
 
     def quit_application(self):
         QApplication.quit()
